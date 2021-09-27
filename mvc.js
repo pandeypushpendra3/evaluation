e55eb22948dfc7ff8dca07fb48c215c32eb87477
pandeypushpendra3 <pandeypushpendra3@gmail.com>
2021-09-27 17:27:46 +0530

    sprint-4/connected schema company to job

--- mvc.js
+++ mvc.js
@@ -36,14 +36,14 @@ app.post("/company",async(req, res)=>{
     const user = await Company.create(req.body);
     return res.send(201).send({user})
 });
-app.get("/company",async(req,res)=>{
-    const user = await Company.find().select({" noticePeriod_time":"90 days"}).lean().exec();
-    return res.send(200).send({user});
+app.get("/companyn",async(req,res)=>{
+    const user = await Company.find().select({"noticePeriod_time":"90days"}).lean().exec();
+    return res.status(200).send({user});
     
 });
 app.get("/company/:id",async(req,res)=>{
     const user = await Company.find(req.params.company_vacancies).lean().exec();
-    return res.send(200).send({user});
+    return res.status(200).send({user});
     
 });
 
@@ -61,7 +61,7 @@ app.post("/job",async(req, res)=>{
 });
 app.get("/job/:id",async(req,res)=>{
     const user = await Job.findById(req.params.working_mode,req.body,{new:true}).lean().exec();
-    return res.send(200).send({user});
+    return res.status(200).send({user});
     
 })
 
